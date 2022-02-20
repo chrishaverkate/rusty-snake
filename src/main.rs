335ab122b154c54417bf489cf4508d5fc35e7dde
Chris Haverkate <chris@techtonium.com>
2022-02-19 17:03:37 -0800

    Parameterizing some game elements.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,16 @@ use rand::Rng;
 use std::collections::LinkedList;
 use std::iter::FromIterator;
 
+const BOARD_WIDTH: i32 = 40; // in number of cells
+const BOARD_HEIGHT: i32 = 40; // in number of cells
+const CELL_SIZE: i32 = 20; // in pixels
+const FREQ_UPDATE: i32 = 20; // The number of loops that must pass before the game updates. Controls the "speed" of the game.
+const FRAMES_PER_SECOND: u64 = 30;
+
+const COLOR_BG: [f32; 4] = [0.0, 0.80, 0.40, 1.0];
+const COLOR_SNAKE: [f32; 4] = [0.9, 0.10, 0.10, 1.0];
+const COLOR_FOOD: [f32; 4] = [0.50, 0.30, 0.0, 1.0];
+
 #[derive(Clone, PartialEq)]
 enum Direction {
 	Right,
@@ -33,11 +43,8 @@ impl Game {
 	fn render(&mut self, arg: &RenderArgs) {
 		// use graphics;
 
-		// RGBA
-		let green: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-
 		self.gl.draw(arg.viewport(), |_c, gl| {
-			graphics::clear(green, gl);
+			graphics::clear(COLOR_BG, gl);
 		});
 
 		self.snake.render(&mut self.gl, arg);
@@ -71,19 +78,19 @@ impl Snake {
 	fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
 		// use graphics;
 
-		let red: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
 		let squares: Vec<graphics::types::Rectangle> = self
 			.body
 			.iter()
-			.map(|&(x, y)| graphics::rectangle::square((x * 20) as f64, (y * 20) as f64, 20_f64))
+			.map(|&(x, y)| {
+				graphics::rectangle::square((x * CELL_SIZE) as f64, (y * CELL_SIZE) as f64, 20_f64)
+			})
 			.collect();
 
 		gl.draw(args.viewport(), |c, gl| {
 			let transform = c.transform;
 			squares
 				.into_iter()
-				.for_each(|square| graphics::rectangle(red, square, transform, gl));
+				.for_each(|square| graphics::rectangle(COLOR_SNAKE, square, transform, gl));
 		});
 	}
 
@@ -119,21 +126,23 @@ impl Food {
 	fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
 		// use graphics;
 
-		let brown: [f32; 4] = [0.5, 0.0125, 0.0, 1.0];
-		let square =
-			graphics::rectangle::square((self.pos_x * 20) as f64, (self.pos_y * 20) as f64, 20_f64);
+		let square = graphics::rectangle::square(
+			(self.pos_x * CELL_SIZE) as f64,
+			(self.pos_y * CELL_SIZE) as f64,
+			20_f64,
+		);
 
 		gl.draw(args.viewport(), |c, gl| {
 			let transform = c.transform;
-			graphics::rectangle(brown, square, transform, gl)
+			graphics::rectangle(COLOR_FOOD, square, transform, gl)
 		});
 	}
 
 	fn update(&mut self) {
 		if self.eaten {
 			self.eaten = false;
-			self.pos_x = rand::thread_rng().gen_range(0..10);
-			self.pos_y = rand::thread_rng().gen_range(0..10);
+			self.pos_x = rand::thread_rng().gen_range(0..BOARD_WIDTH);
+			self.pos_y = rand::thread_rng().gen_range(0..BOARD_HEIGHT);
 			println!("Place new food {}, {}", self.pos_x, self.pos_y);
 		}
 	}
@@ -142,8 +151,11 @@ impl Food {
 fn main() {
 	let opengl = OpenGL::V3_2;
 
+	let width = BOARD_WIDTH * CELL_SIZE;
+	let height = BOARD_HEIGHT * CELL_SIZE;
+
 	// Create an Glutin window.
-	let mut window: GlutinWindow = WindowSettings::new("RustySnake", [200, 200])
+	let mut window: GlutinWindow = WindowSettings::new("RustySnake", [width as f64, height as f64])
 		.graphics_api(opengl)
 		.exit_on_esc(true)
 		.build()
@@ -157,24 +169,31 @@ fn main() {
 			dir: Direction::Right,
 		},
 		food: Food {
-			pos_x: 4,
-			pos_y: 4,
+			pos_x: BOARD_WIDTH / 2,
+			pos_y: BOARD_HEIGHT / 2,
 			eaten: false,
 		},
 	};
 
-	let mut events = Events::new(EventSettings::new()).ups(2);
+	let mut events = Events::new(EventSettings::new()); //.ups(FRAMES_PER_SECOND);
+	let update_freq = FREQ_UPDATE;
+	let mut loop_count = 0;
 
 	// when there is another event in the window...
 	while let Some(e) = events.next(&mut window) {
+		loop_count += 1;
+
 		// ... if that event is a "Render"...
 		if let Some(args) = e.render_args() {
 			// ... render the game
 			game.render(&args);
 		}
 
-		if let Some(_args) = e.update_args() {
-			game.update();
+		if loop_count == update_freq {
+			loop_count = 0;
+			if let Some(_args) = e.update_args() {
+				game.update();
+			}
 		}
 
 		if let Some(args) = e.button_args() {
